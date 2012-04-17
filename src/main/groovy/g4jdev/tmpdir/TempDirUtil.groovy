9e985b156e216c82fb7b0545c097f408b882d61d
Kim A. Betti <kim.betti@gmail.com>
2012-04-17 19:49:34 +0200

    Tmpdir: Java version

--- src/main/groovy/g4jdev/tmpdir/TempDirUtil.groovy
+++ src/main/groovy/g4jdev/tmpdir/TempDirUtil.groovy
@@ -1,16 +1,35 @@
 package g4jdev.tmpdir
 
+import static java.util.UUID.randomUUID
+import org.apache.commons.io.FileUtils
+
 /**
  *  
  * @author Kim A. Betti <kim.betti@gmail.com>
  */
 public class TempDirUtil {
 
-    public static void doWithTemporaryDirectory(TempDirectoryTemplate template) {
+    public static File doWithTemporaryDirectory(TempDirectoryTemplate template) throws IOException {
+        File systemTemporaryDirectory = getSystemTmpDirectory();
+        File temporaryDirectory = new File(systemTemporaryDirectory, randomUUID().toString());
 
+        try {
+            assert temporaryDirectory.mkdir();
+            System.out.println("Created temporary directory " + temporaryDirectory);
+
+            return template.withTemporaryDirectory(temporaryDirectory)
+        }
+        finally {
+            FileUtils.deleteDirectory(temporaryDirectory)
+        }
     }
 
+    private static File getSystemTmpDirectory() {
+        String path = System.getProperty("java.io.tmpdir");
+        File directory = new File(path);
+        assert directory.exists() && directory.isDirectory();
 
-
+        return directory;
+    }
 
 }
--- src/test/groovy/g4jdev/tmpdir/TempDirUtilTest.groovy
+++ src/test/groovy/g4jdev/tmpdir/TempDirUtilTest.groovy
@@ -10,20 +10,19 @@ public class TempDirUtilTest {
 
     @Test
     public void shouldCreateTemporaryDirectory() {
-        File createdDirectory = null;
-        TempDirUtil.doWithTemporaryDirectory(new TempDirectoryTemplate<File>() {
+        File createdDirectory = TempDirUtil.doWithTemporaryDirectory(new TempDirectoryTemplate<File>() {
 
             @Override
             public File withTemporaryDirectory(File temporaryDirectory) {
-                assertTrue(temporaryDirectory.exists());
-                assertTrue(temporaryDirectory.isDirectory());
+                assertTrue("Doesn't exist", temporaryDirectory.exists());
+                assertTrue("Isn't a directory", temporaryDirectory.isDirectory());
 
                 return temporaryDirectory;
             }
 
         });
 
-        assertNotNull(createdDirectory)
+        assertNotNull(createdDirectory);
     }
 
 }
